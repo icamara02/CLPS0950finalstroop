5ff590ba89f413d64944a3d411a92dccb12bbb9d
sgerlein24 <sofia_gerlein@brown.edu>
2024-05-10 17:52:39 -0400

    lucy

--- stroop/stroop experiment_lastrun.py
+++ stroop/stroop experiment_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.1.4),
-    on Fri May 10 15:51:13 2024
+    on Fri May 10 17:47:49 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -131,7 +131,7 @@ def setupData(expInfo, dataDir=None):
     thisExp = data.ExperimentHandler(
         name=expName, version='',
         extraInfo=expInfo, runtimeInfo=None,
-        originPath='/Users/sofiagerlein/Desktop/stroop/stroop experiment_lastrun.py',
+        originPath='/Users/sofiagerlein/Desktop/CLPS0950finalstroop/stroop/stroop experiment_lastrun.py',
         savePickle=True, saveWideText=True,
         dataFileName=dataDir + os.sep + filename, sortColumns='time'
     )
