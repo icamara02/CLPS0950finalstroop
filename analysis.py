af94bdbe59b6216c120ed3be10e7597e9209ca4c
icamara02 <imani_camara@brown.edu>
2024-05-11 14:21:41 -0400

    Update analysis.py
    added more visualization + started code for subgroup analysis

--- analysis.py
+++ analysis.py
@@ -72,7 +72,7 @@ while True:
             plt.plot(data[condition], label=condition, color=colors[i], linestyle[i])
         plt.xlabel('Participant')
         plt.ylabel('Log-Transformed Response Time')
-        plt.title('Log-Tranformed Response Time for Congruent and Incongruent Conditions')
+        plt.title('Log-Transformed Response Time for Congruent and Incongruent Conditions')
         plt.legend(loc='best')
         plt.grid(True)
         plt.show()
@@ -118,10 +118,31 @@ while True:
             print('Invalid input.')
 
 
-
-
-
-
 # Subgroup analysis
 #Looking at data by gender
+data= pd.read_csv('__.csv')
+data['Gender']= data['Gender'].str.lower()
 
+if 'Gender' in data.columns:
+    #group by gender and calculate summary statistics
+    gender_data= data.groupby('Gender')['Response Time'].describe()
+    plt.subplot (1,2,1)
+    plt.table(cellText=gender_data.values,
+              colLabels= gender_data.colummns,
+              rowLabels= gender_data.index,
+              loc='center')
+    plt.axis('off')
+
+    print("Summary statistics for response time grouped by gender: ")
+    print(gender_data)
+    #display bargraph to visualize stats
+    plt.figure(figsize= (12,6))
+    plt.subplot(1,2,2)
+    plt.bar(gender_data.index, gender_data['mean'], color='red')
+    plt.xlabel('Gender')
+    plt.ylabel('Mean Response Time')
+    plt.title('Mean Response Time by Gender')
+
+    plt.show()
+else:
+    print("Error: 'Gender' column not found.")
