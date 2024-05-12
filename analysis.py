fce31562c6b9996c8dd965eafee49c31a20c8182
icamara02 <imani_camara@brown.edu>
2024-05-12 14:45:22 -0400

    Update analysis.py
    wrote code for data anlysis + visualization from pivot table

--- analysis.py
+++ analysis.py
@@ -1,27 +1,26 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
 
 #read data from CSV
-data = pd.read_csv('__.csv')
+data = pd.read_csv('pivot_table.csv')
 
 #Descriptives statistics table
 while True:
-    descriptives_prompt = 'Do you want to see a descriptives table?(yes/no)'
-    if descriptives_prompt.lower()== 'yes':
-        group_data= data[['']]
-        descripitve_table = pd.DataFrame({
-            'Mean': condition_data.mean(),
-            'Median': condition_data.median(),
-            'StdDev': condition_data.std(),
-            'Min': condition_data.min(),
-            'Max': condition_data.max(),
-        }, index= [''])
-        print(descriptives_table)
+    descriptives_prompt = input('Do you want to see a descriptives table?(yes/no)')
+    if descriptives_prompt == 'yes':
+        group_data = data[['GC_Cong_Avg', 'GC_Incong_Avg', 'CN_Cong_Avg', 'CN_Incong_Avg']]
+        descriptive_table = pd.DataFrame({
+            'Mean': group_data.mean(),
+            'Median': group_data.median(),
+            'StdDev':group_data.std(),
+            'Min': group_data.min(),
+            'Max': group_data.max(),
+        }, index= ['GC_Cong_Avg', 'GC_Incong_Avg', 'CN_Cong_Avg', 'CN_Incong_Avg'])
+        print(descriptive_table)
         break
-    elif descripitives_prompt.lower() == 'no':
+    elif descriptives_prompt == 'no':
         print('Descripitves table not requested.')
         break
     else:
@@ -29,14 +28,14 @@ while True:
 
 #Repeated-Measures ANOVA
 while True:
-    anova_prompt = 'Do you want to perform a repeated-measures ANOVA? (yes/no)'
-    choice = input(anova_prompt).lower()
-    if choice == 'yes':
-        within_subjects= []
-        rm= statsmodels.stats.anova.AnovaRM(data, )
-        print(rm.fit())
+    anova_prompt = input('Do you want to perform a repeated-measures ANOVA? (yes/no)')
+    if anova_prompt == 'yes':
+        within_subjects= ['GC_Cong_Avg', 'GC_Incong_Avg', 'CN_Cong_Avg', 'CN_Incong_Avg']
+        rm_data= data.melt(var_name= 'Condition', value_name= 'Response Time')
+        rm_data['Participant']= range(1, len(rm_data)+1)
+        print(rm_data)
         break
-    elif choice == 'no':
+    elif anova_prompt == 'no':
         print('Repeated-measures ANOVA not requested.')
         break
     else:
@@ -44,16 +43,15 @@ while True:
 
 #One-way Anova
 while True:
-    oneWay_prompt= 'Do you want to perform one-way ANOVA? (yes/no)'
-    choice= input(oneWay_prompt).lower()
-    if choice == 'yes':
-        conditions= []
+    oneWay_prompt= input('Do you want to perform one-way ANOVA? (yes/no)')
+    if oneWay_prompt == 'yes':
+        conditions= ['GC_Cong_Avg', 'GC_Incong_Avg', 'CN_Cong_Avg', 'CN_Incong_Avg']
         response_times = data[conditions]
         f_statistic, p_value= stats.f_oneway(*[data[col] for col in response_times.columns])
         print('F-statisitic:', f_statistic)
         print('p-value:', p_value)
         break
-    elif choice == 'no':
+    elif oneWay_prompt == 'no':
         print('One-way ANOVA not requested.')
         break
     else:
@@ -61,15 +59,14 @@ while True:
 
 #Log-transformed Graph
 while True:
-    graph_prompt= 'Do you want to produce a graph with average response time between conditions? (yes/no)'
-    choice= input(graph_prompt).lower() == 'yes':
-    if graph_prompt.lower ()== 'yes':
-        condition= []
+    graph_prompt= input ('Do you want to produce a graph with average response time between conditions? (yes/no)')
+    if graph_prompt== 'yes':
+        condition= ['GC_Cong_Avg', 'GC_Incong_Avg', 'CN_Cong_Avg', 'CN_Incong_Avg']
         colors = ['b', 'r', 'g', 'm']
         linestyle = ['-','--', '-', '--']
         plt.figure()
         for i, condition in enumerate(conditions):
-            plt.plot(data[condition], label=condition, color=colors[i], linestyle[i])
+            plt.plot(data[condition], linestyle[i], label=condition, color=colors[i])
         plt.xlabel('Participant')
         plt.ylabel('Log-Transformed Response Time')
         plt.title('Log-Transformed Response Time for Congruent and Incongruent Conditions')
@@ -77,72 +74,29 @@ while True:
         plt.grid(True)
         plt.show()
         break
-    elif graph_prompt.lower ()== 'no':
+    elif graph_prompt== 'no':
         print('Graph not requested.')
         break
     else:
         print('Invalid input. Please enter "yes" or "no".')
 
 #Box plot
+while True:
     boxplot_prompt= input('Do you want to see a box plot?(yes/no)')
-    if boxplot_prompt.lower()== 'yes':
-        response_times= [data[condition] for condition in conditions]
+    condition = ['GC_Cong_Avg', 'GC_Incong_Avg', 'CN_Cong_Avg', 'CN_Incong_Avg']
+    if boxplot_prompt== 'yes':
+        response_times= [data[condition] for condition in condition]
         plt.figure()
-        plt.boxplot(response_times, labels= conditions)
+        plt.boxplot(response_times, labels= condition)
         plt.xlabel('Task and Condition')
         plt.ylabel('Response Time')
         plt.title('Box Plot of Response Times for Different Tasks and Condtions')
         plt.show()
         break
-    elif boxplot_prompt.lower()== 'no':
+    elif boxplot_prompt== 'no':
         print('Box plot not requested.')
         break
     else:
         print('Please enter a valid choice.')
 
-    #Histogram
-    while True:
-        hist_prompt= input('Do you want to see a histogram of log-transformed response times? (yes/no)')
-        if hist_prompt.lower()== 'yes':
-            plt.figure()
-            plt.hist(np.log(data['responseTime']))
-            plt.xlabel('Log-Transformed Response Time')
-            plt.ylabel('Frequency')
-            plt.title('Histogram of Log-Transformed Response Time')
-            plt.show()
-            break
-        elif hist_prompt.lower()== 'no':
-            print ('Histogram not requested.')
-            break
-        else:
-            print('Invalid input.')
 
-
-# Subgroup analysis
-#Looking at data by gender
-data= pd.read_csv('__.csv')
-data['Gender']= data['Gender'].str.lower()
-
-if 'Gender' in data.columns:
-    #group by gender and calculate summary statistics
-    gender_data= data.groupby('Gender')['Response Time'].describe()
-    plt.subplot (1,2,1)
-    plt.table(cellText=gender_data.values,
-              colLabels= gender_data.colummns,
-              rowLabels= gender_data.index,
-              loc='center')
-    plt.axis('off')
-
-    print("Summary statistics for response time grouped by gender: ")
-    print(gender_data)
-    #display bargraph to visualize stats
-    plt.figure(figsize= (12,6))
-    plt.subplot(1,2,2)
-    plt.bar(gender_data.index, gender_data['mean'], color='red')
-    plt.xlabel('Gender')
-    plt.ylabel('Mean Response Time')
-    plt.title('Mean Response Time by Gender')
-
-    plt.show()
-else:
-    print("Error: 'Gender' column not found.")
