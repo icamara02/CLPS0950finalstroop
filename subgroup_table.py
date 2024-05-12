3d7f9c9ac9f338c82c90df0518748f9dfa610e4b
icamara02 <imani_camara@brown.edu>
2024-05-12 19:23:46 -0400

    merge data from pivot table and questionnaire for subgroupanalysis

---
+++ subgroup_table.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Read the first CSV file
+df1 = pd.read_csv('pivot_table.csv')
+
+# Read the second CSV file
+df2 = pd.read_csv('filtered_survey.csv')
+
+# Merge the two DataFrames based on a common identifier (e.g., participant ID)
+merged_df = pd.merge(df1, df2, left_on='participant id', right_on='Participant ID')
+
+# Save the merged DataFrame to a new CSV file
+merged_df.to_csv('subgroup_merged.csv', index=False)
